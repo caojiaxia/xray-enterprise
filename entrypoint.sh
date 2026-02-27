804711ab7ef529a0edc208c2047907a59b7b33ee
caojiaxia <104258329+caojiaxia@users.noreply.github.com>
2026-02-27 17:14:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,13 @@
 
+#!/bin/sh
+
+UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
+WSPATH=${WSPATH:-ws}
+
+sed "s/__UUID__/$UUID/g" /template.json | \
+sed "s/__WSPATH__/$WSPATH/g" > /etc/xray/config.json
+
+echo "UUID: $UUID"
+echo "WS Path: /$WSPATH"
+
+exec xray -config /etc/xray/config.json
