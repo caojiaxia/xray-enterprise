1b4b9a9743e65a8e4de8e55738ccd45df976ffea
caojiaxia <104258329+caojiaxia@users.noreply.github.com>
2026-02-28 20:51:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-# 设置默认值（如果环境变量为空）
 export UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
-export XPATH=${XPATH:-/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)}
+export XPATH=${XPATH:-/randompath}
 
-echo "正在启动 Xray..."
-echo "当前使用的 UUID: $UUID"
-echo "当前使用的 PATH: $XPATH"
-
-# 使用 envsubst 替换模板中的变量并生成最终配置文件
+# 这一步会将 template 转换为真正的 config.json
 envsubst '${UUID},${XPATH}' < /etc/xray/config.template.json > /etc/xray/config.json
 
-# 启动 Xray
 exec /usr/bin/xray -config /etc/xray/config.json
